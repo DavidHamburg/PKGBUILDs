bdc2bb6f7ffced069e02116eccecf5fb5ebe49ef
David Nerjes <david.nerjes@mailbox.org>
2017-12-18 04:05:39 +0100

    add more packages

--- update.sh
+++ update.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 for i in */
 do
-  cd $i
-  git checkout master
-  git pull origin master
-  cd -
+    if [ "$i" != "testing/" ] && [ "$i" != "patches/" ]
+    then
+        cd $i
+        git checkout master
+        git pull origin master
+        cd -
+    fi
 done
+#cd testing
+#for i in */
+#do
+#    if [ "$i" != "patches/" ]
+#    then
+#        cd $i
+#        git checkout master
+#        git pull origin master
+#        cd -
+#    fi
+#done
